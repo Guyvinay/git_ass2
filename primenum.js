e90cc4b31055731c5f015b1e7b8f950107191455
Guyvinay <mrsinghvinay563@gmail.com>
2022-11-30 20:23:02 +0530

    fix_it_Yogesh

---
+++ primenum.js
@@ -0,0 +1,10 @@
+let num=23;
+let count=0;
+for(i=0;i<=23;i++){
+  if(nuuum%i==0){
+    count++
+            }
+}if(count==2){
+  console.log("It is a Prime NUmber")
+}else {
+  console.log("It's Not")
